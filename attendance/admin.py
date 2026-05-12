57ba94fc882c97d79a9ff8c8f954c992add9f74e
Sorin Hus <sorin.hus@gmail.com>
2026-05-12 09:08:47 +0300

    fix: register AttendanceSession in Django admin

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Attendance
+from .models import Attendance, AttendanceSession
 
 
 @admin.register(Attendance)
@@ -7,3 +7,11 @@ class AttendanceAdmin(admin.ModelAdmin):
     list_display = ['user', 'date', 'check_in', 'check_out', 'work_hours', 'status']
     list_filter = ['status', 'date']
     search_fields = ['user__username', 'user__first_name', 'user__last_name']
+
+
+@admin.register(AttendanceSession)
+class AttendanceSessionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date', 'clock_in', 'clock_out', 'work_hours', 'status']
+    list_filter = ['status', 'date']
+    search_fields = ['user__username', 'user__first_name', 'user__last_name']
+    ordering = ['-date', '-clock_in']
